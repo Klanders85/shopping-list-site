ec8a2ad31e2889ab30a89042892551f95a21f5e0
Kelly Landers <kelly.landers@sq1.com>
2014-05-28 20:29:23 -0500

    adding jQuery scripts for list functionality

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// var grocery_items = {
+// 	produce:{
+// 		fruit:[
+// 			"apples",
+// 			"oranges"
+// 		],
+// 		veggies:[
+// 			"carrots",
+// 			"cucumbers"
+// 		]
+// 	},
+// 	proteins:{
+// 		chicken:[
+// 			"drumsticks",
+// 			"thighs",
+// 			"breasts"
+// 		],
+// 		pork:[
+// 			"tenderloin",
+// 			"bacon"
+// 		],
+// 		beef:[
+// 			"ribeye",
+// 			"strip",
+// 			"ribs"
+// 		],
+// 	},
+// 	cleaners:[
+// 		"paper-towels",
+// 		"trash bags",
+// 		"toilet bowl cleaner",
+// 		"air freshener",
+// 		"windex"
+// 	]
+// };
